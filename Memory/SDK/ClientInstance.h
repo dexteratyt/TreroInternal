bdfe36799ed0d16211e4d90ba9d3101e05909833
Laamy <shade4keal@gmail.com>
2021-09-16 03:54:44 +1000

    attempting to import sig scanning

--- Memory/SDK/ClientInstance.h
+++ Memory/SDK/ClientInstance.h
@@ -1,9 +1,12 @@
 #include <Windows.h>
+#include "../Utils.h"
 
 class ClientInstance {
 public:
-    class Player* getLongP(){
-        return *reinterpret_cast<Player**>((uintptr_t)(this) + 0x140);
+    class Player* getLocalPlayer(){
+		static unsigned int offset = 0x140;
+		offset = *reinterpret_cast<int*>(Mem::findSig("48 8B 81 ? ? ? ? C3 CC CC CC CC CC CC CC CC 40 55") + 3);
+        return *reinterpret_cast<Player**>((uintptr_t)(this) + offset); // 0x140
     };
 
     bool* getKey(char c) {
